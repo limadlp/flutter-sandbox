55256c6c5b0fd64155b746164113b7d0396599fe
limadlp <dlplima@hotmail.com>
2025-02-18 00:44:42 -0300

    refactor: use bloc packages

--- bloc_sandbox/lib/blocs/cient_bloc.dart
+++ bloc_sandbox/lib/blocs/cient_bloc.dart
@@ -1,32 +1,53 @@
-import 'dart:async';
-
+import 'package:bloc/bloc.dart';
 import 'package:bloc_sandbox/blocs/client_events.dart';
 import 'package:bloc_sandbox/blocs/client_state.dart';
-import 'package:bloc_sandbox/models/client.dart';
 import 'package:bloc_sandbox/repositories/clients_repository.dart';
 
-class ClientBloc {
+//  With bloc package
+class ClientBloc extends Bloc<ClientEvent, ClientState> {
   final _clientRepo = ClientsRepository();
 
-  final _inputClientController = StreamController<ClientEvent>();
-  final _outputClientController = StreamController<ClientState>();
-
-  Sink<ClientEvent> get inputClient => _inputClientController.sink;
-  Stream<ClientState> get stream => _outputClientController.stream;
-
-  ClientBloc() {
-    _inputClientController.stream.listen(_mapEventToState);
-  }
-
-  _mapEventToState(ClientEvent event) async {
-    List<Client> clients = [];
-    if (event is LoadClientsEvent) {
-      clients = _clientRepo.loadClients();
-    } else if (event is AddClientEvent) {
-      clients = _clientRepo.addClient(event.client);
-    } else if (event is RemoveClientEvent) {
-      clients = _clientRepo.removeClient(event.client);
-    }
-    _outputClientController.add(ClientSuccessState(clients: clients));
+  ClientBloc() : super(ClientInitialState()) {
+    on<LoadClientsEvent>(
+      (event, emit) =>
+          emit(ClientSuccessState(clients: _clientRepo.loadClients())),
+    );
+    on<AddClientEvent>(
+      (event, emit) => emit(
+        ClientSuccessState(clients: _clientRepo.addClient(event.client)),
+      ),
+    );
+    on<RemoveClientEvent>(
+      (event, emit) => emit(
+        ClientSuccessState(clients: _clientRepo.removeClient(event.client)),
+      ),
+    );
   }
 }
+
+// Without bloc package
+// class ClientBloc {
+//   final _clientRepo = ClientsRepository();
+
+//   final _inputClientController = StreamController<ClientEvent>();
+//   final _outputClientController = StreamController<ClientState>();
+
+//   Sink<ClientEvent> get inputClient => _inputClientController.sink;
+//   Stream<ClientState> get stream => _outputClientController.stream;
+
+//   ClientBloc() {
+//     _inputClientController.stream.listen(_mapEventToState);
+//   }
+
+//   _mapEventToState(ClientEvent event) async {
+//     List<Client> clients = [];
+//     if (event is LoadClientsEvent) {
+//       clients = _clientRepo.loadClients();
+//     } else if (event is AddClientEvent) {
+//       clients = _clientRepo.addClient(event.client);
+//     } else if (event is RemoveClientEvent) {
+//       clients = _clientRepo.removeClient(event.client);
+//     }
+//     _outputClientController.add(ClientSuccessState(clients: clients));
+//   }
+// }
--- bloc_sandbox/lib/pages/clients_page.dart
+++ bloc_sandbox/lib/pages/clients_page.dart
@@ -5,6 +5,7 @@ import 'package:bloc_sandbox/blocs/client_events.dart';
 import 'package:bloc_sandbox/blocs/client_state.dart';
 import 'package:bloc_sandbox/models/client.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ClientsPage extends StatefulWidget {
   const ClientsPage({super.key});
@@ -20,12 +21,12 @@ class _ClientsPageState extends State<ClientsPage> {
   void initState() {
     super.initState();
     bloc = ClientBloc();
-    bloc.inputClient.add(LoadClientsEvent());
+    bloc.add(LoadClientsEvent());
   }
 
   @override
   void dispose() {
-    bloc.inputClient.close();
+    bloc.close();
     super.dispose();
   }
 
@@ -53,9 +54,7 @@ class _ClientsPageState extends State<ClientsPage> {
         actions: [
           IconButton(
             onPressed: () {
-              bloc.inputClient.add(
-                AddClientEvent(client: Client(nome: randomName())),
-              );
+              bloc.add(AddClientEvent(client: Client(nome: randomName())));
             },
             icon: Icon(Icons.person_add),
           ),
@@ -63,35 +62,66 @@ class _ClientsPageState extends State<ClientsPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 24),
-        child: StreamBuilder<ClientState>(
-          stream: bloc.stream,
-          builder: (context, snapshot) {
-            final clientsList = snapshot.data?.clients ?? [];
-            return ListView.separated(
-              itemCount: clientsList.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    child: Text(clientsList[index].nome[0].toUpperCase()),
-                  ),
-                  title: Text(clientsList[index].nome),
-                  trailing: IconButton(
-                    onPressed: () {
-                      bloc.inputClient.add(
-                        RemoveClientEvent(client: clientsList[index]),
-                      );
-                    },
-                    icon: Icon(Icons.delete),
-                  ),
-                );
-              },
-              separatorBuilder: (context, index) {
-                return const Divider();
-              },
-            );
+        child: BlocBuilder<ClientBloc, ClientState>(
+          bloc: bloc,
+          builder: (context, state) {
+            if (state is ClientInitialState) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is ClientSuccessState) {
+              final clientsList = state.clients;
+              return ListView.separated(
+                itemCount: clientsList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      child: Text(clientsList[index].nome[0].toUpperCase()),
+                    ),
+                    title: Text(clientsList[index].nome),
+                    trailing: IconButton(
+                      onPressed: () {
+                        bloc.add(RemoveClientEvent(client: clientsList[index]));
+                      },
+                      icon: Icon(Icons.delete),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return const Divider();
+                },
+              );
+            }
+            return Container();
           },
         ),
       ),
     );
   }
 }
+
+        // Without flutter bloc package
+        // child: StreamBuilder<ClientState>(
+        //   stream: bloc.stream,
+        //   builder: (context, snapshot) {
+        //     final clientsList = snapshot.data?.clients ?? [];
+        //     return ListView.separated(
+        //       itemCount: clientsList.length,
+        //       itemBuilder: (context, index) {
+        //         return ListTile(
+        //           leading: CircleAvatar(
+        //             child: Text(clientsList[index].nome[0].toUpperCase()),
+        //           ),
+        //           title: Text(clientsList[index].nome),
+        //           trailing: IconButton(
+        //             onPressed: () {
+        //               bloc.add(RemoveClientEvent(client: clientsList[index]));
+        //             },
+        //             icon: Icon(Icons.delete),
+        //           ),
+        //         );
+        //       },
+        //       separatorBuilder: (context, index) {
+        //         return const Divider();
+        //       },
+        //     );
+        //   },
+        // ),
