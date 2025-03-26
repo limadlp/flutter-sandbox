9a942e348046339b80b17835fb28d538083164f8
limadlp <dlplima@hotmail.com>
2025-03-26 00:17:26 -0300

    password validation initial config

---
+++ password_validation/android/app/src/main/kotlin/dev/danlima/password_validation/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.danlima.password_validation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ password_validation/lib/src/pages/validation_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ValidationPage extends StatelessWidget {
+  const ValidationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pages')),
+      body: Center(child: Text('Validation Page')),
+    );
+  }
+}
