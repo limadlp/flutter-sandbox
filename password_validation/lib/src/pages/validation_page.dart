24ee1de4422454707457744b401a72b4eaf91a08
limadlp <dlplima@hotmail.com>
2025-03-30 01:05:46 -0300

    password_validation: create base layout

--- password_validation/lib/src/pages/validation_page.dart
+++ password_validation/lib/src/pages/validation_page.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:password_validation/src/shared/colors_app.dart';
 
 class ValidationPage extends StatelessWidget {
   const ValidationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Pages')),
-      body: Center(child: Text('Validation Page')),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 44, bottom: 42),
+                child: Image.asset('assets/images/logo.png'),
+              ),
+              Text(
+                'Defina uma senha',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: primaryColor,
+                ),
+              ),
+              Text(
+                'Uma senha forte ajuda a manter sua conta segura',
+                style: TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600,
+                  color: mediumGreyColor,
+                ),
+              ),
+              SizedBox(height: 26),
+              TextFormField(),
+              SizedBox(height: 26),
+              TextFormField(),
+              SizedBox(height: 37),
+              Container(height: 146, color: Colors.grey),
+              SizedBox(height: 40),
+              ElevatedButton(onPressed: () {}, child: Text('Senha Reprovada')),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ password_validation/lib/src/shared/colors_app.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xff7132C4);
+
+const mediumGreyColor = Color(0xff9C9C9C);
