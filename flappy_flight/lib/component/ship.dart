46376919a7424853bcc81b71cf375e09d969d5af
Dangeles Lima <dlplima@hotmail.com>
2025-11-16 20:16:37 -0300

    add pipes

--- flappy_flight/lib/component/ship.dart
+++ flappy_flight/lib/component/ship.dart
@@ -4,18 +4,20 @@ import 'dart:ui';
 import 'package:flame/components.dart';
 
 class Ship extends PositionComponent {
-  Ship()
+  final double maxY;
+  Ship({required this.maxY})
     : super(
         position: Vector2(0, 0),
         size: Vector2.all(80),
         anchor: Anchor.center,
+        priority: 10,
       );
 
   late Sprite _dashSprite;
 
-  final Vector2 _gravity = Vector2(0, 900); 
+  final Vector2 _gravity = Vector2(0, 1400);
   Vector2 _velocity = Vector2(0, 0);
-  final Vector2 _jumpForce = Vector2(0, -400);
+  final Vector2 _jumpForce = Vector2(0, -500);
 
   @override
   Future<void> onLoad() async {
@@ -33,6 +35,14 @@ class Ship extends PositionComponent {
     super.update(dt);
     _velocity += _gravity * dt;
     position += _velocity * dt;
+
+    if (position.y < -maxY) {
+      position.y = -maxY;
+      _velocity.y = 0;
+    } else if (position.y > maxY) {
+      position.y = maxY;
+      _velocity.y = 0;
+    }
   }
 
   @override
--- flappy_flight/lib/flappy_flight_game.dart
+++ flappy_flight/lib/flappy_flight_game.dart
@@ -1,10 +1,15 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flappy_flight/component/flight_parallax_background.dart';
+import 'package:flappy_flight/component/pipe_pair.dart';
 import 'package:flappy_flight/component/ship.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
-class FlappyFlightGame extends FlameGame<FlappyFlightWorld> {
+class FlappyFlightGame extends FlameGame<FlappyFlightWorld> with KeyboardEvents {
   FlappyFlightGame()
     : super(
         world: FlappyFlightWorld(),
@@ -13,21 +18,76 @@ class FlappyFlightGame extends FlameGame<FlappyFlightWorld> {
           height: 1000,
         ),
       );
+
+  @override
+  KeyEventResult onKeyEvent(
+    KeyEvent event,
+    Set<LogicalKeyboardKey> keysPressed,
+  ) {
+    final isKeyDown = event is KeyDownEvent;
+
+    final isSpace = keysPressed.contains(LogicalKeyboardKey.space);
+
+    if (isSpace && isKeyDown) {
+      world.onSpaceDown();
+      return KeyEventResult.handled;
+    }
+    return KeyEventResult.ignored;
+  }
 }
 
-class FlappyFlightWorld extends World with TapCallbacks {
+class FlappyFlightWorld extends World with TapCallbacks, HasGameReference<FlappyFlightGame> {
   late Ship _ship;
+  late PipePair _lastPipe;
+  static const _pipesDistance = 400.0;
 
   @override
   void onLoad() {
     super.onLoad();
+
     add(FligthParallaxBackground());
-    add(_ship = Ship());
+    add(_ship = Ship(maxY: 1000));
+    _generatePipes(fromX: -350);
+  }
+
+  void _generatePipes({
+    int count = 5,
+    double fromX = 0.0,
+  }) {
+    for (int i = 0; i < count; i++) {
+      const area = 600;
+      final y = (Random().nextDouble() * area) - (area / 2);
+      add(_lastPipe = PipePair(position: Vector2(fromX + i * _pipesDistance, y)));
+    }
+  }
+
+  void _removePipes() {
+    final pipes = children.whereType<PipePair>();
+    final shouldBeRemoved = max(pipes.length - 5, 0);
+    pipes.take(shouldBeRemoved).forEach((pipe) {
+      pipe.removeFromParent();
+    });
   }
 
   @override
   void onTapDown(TapDownEvent event) {
     super.onTapDown(event);
     _ship.jump();
+  }
+
+  void onSpaceDown() {
+    _ship.jump();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (_ship.x >= _lastPipe.x) {
+      _generatePipes(
+        fromX: _pipesDistance,
+      );
+      _removePipes();
+    }
+    //game.camera.viewfinder.zoom = 0.3;
   }
 }
